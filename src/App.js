a1c2858b981da78f308fa1e2854c0f637ada0ebc
kamille <k.pagibigan@gmail.com>
2021-03-05 22:22:27 -0700

    delete test data from App.js file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3000/notebooks')
-  //   .then(response => response.json())
-  //   .then(data => console.log(data))
-  // }
-
-
   render () {
     return (
       <div>
