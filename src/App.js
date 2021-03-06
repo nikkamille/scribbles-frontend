47e8e3af2b8783cb62e9d2a837b8340d8a00e216
kamille <k.pagibigan@gmail.com>
2021-03-05 21:04:39 -0700

    test fetch request

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/notebooks')
-    .then(response => response.json())
-    .then(data => console.log(data[0].notes))
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/notebooks')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
 
   render () {
