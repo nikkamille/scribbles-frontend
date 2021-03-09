3c6edb77175c1ac7b219f8739328b86fe9e1732b
kamille <k.pagibigan@gmail.com>
2021-03-08 23:30:24 -0700

    install redux devtools extension, import composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import './index.css';
 import App from './App';
 
-let reducer = ''
-
-let store = createStore(reducer, applyMiddleware(thunk))
+const initialState = {}
+let store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), composeWithDevTools())) 
+// create a reducers folder; create a rootReducer file inside and import the other reducers and do combineReducers
 
 ReactDOM.render(
   <Provider>
