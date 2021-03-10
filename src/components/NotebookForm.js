b427b22f82d03d33311e73c56734cf438fc70d91
kamille <k.pagibigan@gmail.com>
2021-03-09 23:12:00 -0700

    create NotebookForm component

---
+++ src/components/NotebookForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotebookForm extends Component {
+    render() {
+        return(
+            <div>
+                Input notebook here!
+            </div>
+        )
+    }
+}
+
+export default NotebookForm;
