34b06d939d65198e79d4d4d2d59a3ce48074e4c5
kamille <k.pagibigan@gmail.com>
2021-03-09 22:10:06 -0700

    create NotebooksContainer component

---
+++ src/containers/NotebooksContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'redux';
+
+class NotebooksContainer extends Component {
+    render() {
+        return(
+            <div>
+                I'm in my NotebooksContainer!
+            </div>
+        )
+    }
+}
+
+export default NotebooksContainer
