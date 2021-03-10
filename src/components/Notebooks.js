170cdaec768694d5c1f29980df89d52ac47660e2
kamille <k.pagibigan@gmail.com>
2021-03-09 23:07:43 -0700

    create Notebooks component

---
+++ src/components/Notebooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Notebooks extends Component {
+    render() {
+        return(
+            <div>
+                List of notebooks
+            </div>
+        )
+    }
+}
+
+export default Notebooks;
