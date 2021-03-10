58b65e406fe208795f36254b73255451485118b3
kamille <k.pagibigan@gmail.com>
2021-03-09 23:10:02 -0700

    import Notebooks into NotebooksContainer

--- src/containers/NotebooksContainer.js
+++ src/containers/NotebooksContainer.js
@@ -1,11 +1,13 @@
 // This will be a route '/notebooks'
 import React, { Component } from 'react';
+import Notebooks from '../components/Notebooks';
 
 class NotebooksContainer extends Component {
     render() {
         return(
             <div>
                 I'm in my NotebooksContainer!
+                <Notebooks/>
             </div>
         )
     }
