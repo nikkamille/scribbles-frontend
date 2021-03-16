af78372d98b69628744b84b83169cc3a9b3cd80e
kamille <k.pagibigan@gmail.com>
2021-03-15 23:58:25 -0600

    create notes container

---
+++ src/containers/NotesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotesContainer extends Component {
+    render() {
+        return (
+            <div>
+                My Notes Container!
+            </div>
+        )
+    }
+}
+
+export default NotesContainer
