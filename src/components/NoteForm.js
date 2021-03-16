d028601b88df908995db2b5fff36d2221b1a3697
kamille <k.pagibigan@gmail.com>
2021-03-16 14:49:31 -0600

    change input tag to textarea tag in NoteForm

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class NoteForm extends Component {
+    
+    state = {
+        title: "",
+        content: "",
+        date: ""
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })        
+    }
+
     render() {
         return (
             <div>
                 Write your notes here:
+                <form>
+                    <input type="text" placeholder="Title" name="title" onChange={this.handleChange}/><br/><br/>
+                    <textarea type="textarea" placeholder="Start writing" name="content" rows={15} cols={100} onChange={this.handleChange}/><br/><br/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
 }
 
-export default NoteForm
+export default connect(null)(NoteForm)
