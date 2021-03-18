645484d11353bc085261a3f36229547a573d64fd
kamille <k.pagibigan@gmail.com>
2021-03-18 03:13:00 -0600

    update route for Note component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Note from './Note';
 
 function Notes(props) {
@@ -12,12 +12,14 @@ function Notes(props) {
                 {props.notes && props.notes.map(note => 
                     <li key={note.id}>
                         {/* <Note note={note}/> */}
-                        {note.title} - {note.content}
-                        {/* <Link to={`/notebooks/${note.notebook_id}/notes/${note.id}`}>{note.title}</Link> */}
+                        {/* {note.title} - {note.content} */}
+                        <Link to={`/notebooks/${note.notebook_id}/notes/${note.id}`}>{note.title}</Link>
                         {/* Uncomment the link part when I'm doing the routing */}
                     </li>
                 )}
             </ul>
+            {/* <Route exact path="/notebooks/:id/notes/:id" render={(routerProps) => <Note {...routerProps}/>}/> */}
+                {/* <Note note={props.notes.map(note => note.title, note.content)}/> */}
         </div>
     )
 
--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 import Notes from '../components/Notes';
 import NoteForm from '../components/NoteForm';
+import Note from '../components/Note';
 
 class NotesContainer extends Component {
     
     render() {
-        // debugger
+        debugger
         return (
             <div>
                 My Notes Container!
                 <NoteForm notebook={this.props.notebook}/>
                 <Notes notes={this.props.notebook ? this.props.notebook.notes : null}/>
+                <Route exact path="/notebooks/:id/notes/:id" render={(routerProps) => <Note {...routerProps}/>}/>
             </div>
         )
     }
