ecd52a44ca348ec0775d6a6db1fae57ecab52372
kamille <k.pagibigan@gmail.com>
2021-03-18 18:41:46 -0600

    update routes

--- src/containers/NotebooksContainer.js
+++ src/containers/NotebooksContainer.js
@@ -15,6 +15,7 @@ class NotebooksContainer extends Component {
     }
     
     render() {
+    debugger
         return(
             <div>
                 <Switch>
--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -8,13 +8,14 @@ import Note from '../components/Note';
 class NotesContainer extends Component {
     
     render() {
-        debugger
+        // debugger
         return (
             <div>
                 My Notes Container!
                 <NoteForm notebook={this.props.notebook}/>
                 <Notes notes={this.props.notebook ? this.props.notebook.notes : null}/>
                 <Route exact path="/notebooks/:id/notes/:id" render={(routerProps) => <Note {...routerProps}/>}/>
+                {/* <Route exact path="/notebooks/:id" render={((routerProps) => <Notebook notebook={this.props.notebooks.find(notebook => notebook.id === parseInt(routerProps.match.params.id))} /> ) }/> */}
             </div>
         )
     }
