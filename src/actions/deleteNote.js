0cc24ba6eee4ce1bf701845d6e9b98b67241d4d5
kamille <k.pagibigan@gmail.com>
2021-03-19 05:15:11 -0600

    create deleteNote action creator

---
+++ src/actions/deleteNote.js
@@ -0,0 +1,13 @@
+export default function deleteNote(notebookId, noteId) {
+
+    return (dispatch) => {
+        return fetch(`http://localhost:3000/notebooks/${notebookId}/notes/${noteId}`, {
+            method: "DELETE"
+        })
+        .then(response => response.json())
+        .then(note => {
+            dispatch({type: "DELETE_LIST", payload: noteId})
+        })
+    }
+     
+}
