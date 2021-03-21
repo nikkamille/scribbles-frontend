6c2eb3c03cb128802bf6a94d18367cb2c9f59777
kamille <k.pagibigan@gmail.com>
2021-03-21 16:16:46 -0600

    add alert message when title form for notebook is empty.

--- src/actions/addNotebook.js
+++ src/actions/addNotebook.js
@@ -12,8 +12,13 @@ export default function addNotebook(notebook) {
         fetch('http://localhost:3000/notebooks', options)
         .then(response => response.json())
         .then(notebook => {
+            if (notebook.error) {
+                alert(notebook.error)
+            } else {
             dispatch({type: 'ADD_NOTEBOOK', payload: notebook})
+            }
         })
+        // I may remove the alert when I'm able to add a default value for notebook if title is left empty.
         
     }
 }
