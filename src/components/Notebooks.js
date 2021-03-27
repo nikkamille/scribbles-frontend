2e910e4b70a298de654dffa10c0aba7bdf3a3184
kamille <k.pagibigan@gmail.com>
2021-03-27 05:53:53 -0600

    fix styling of Notebooks component

--- src/components/Notebooks.js
+++ src/components/Notebooks.js
@@ -6,18 +6,23 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import { IconButton } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 const styles = makeStyles({
     root: {
+        textAlign: 'center',
+        width: 200,
+        height: 120,
+        borderColor: '#5D6B83',
+        margin: 'auto',
+        padding: '10px',
+        backgroundColor: '#FAF0F1'
+    },
+    content: {
         minWidth: 275,
         textDecoration: 'none',
-        textAlign: 'center',
-        fontWeight: 500,
-        border: '1px solid',
-        borderColor: '#5D6B83'
+        fontWeight: 500
       }
     
 })
@@ -30,20 +35,10 @@ function Notebooks(props) {
         <Container>
             <Box m={4}>
                 <Grid container spacing={3}>
-                    {/* <Grid item xs={3}>
-                        <Card component={Link} to={"/notebooks/new"} variant="outlined" raised="true" className={classes.root}>
-                            <CardContent>
-                                <IconButton>
-                                    <AddCircleIcon/>
-                                </IconButton>
-                            </CardContent>
-                        </Card>
-                    </Grid> */}
                     {props.notebooks.notebooks && props.notebooks.notebooks.map(notebook => 
                     <Grid item xs={3}>        
-                        <Card component={Link} to={`/notebooks/${notebook.id}`} className={classes.root} variant="outlined">
-                            <CardContent>{notebook.title}</CardContent>
-                            {/* <Link to={`/notebooks/${notebook.id}`}>{notebook.title}</Link> */}
+                        <Card variant="outlined" className={classes.root}>
+                            <CardContent component={Link} to={`/notebooks/${notebook.id}`} className={classes.content}>{notebook.title}</CardContent>
                         </Card>
                     </Grid>
                     )}
